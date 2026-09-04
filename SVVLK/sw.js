a7b746266ac047fedd681475a410d6639ab56b9c
meghan2007 <meghan4167@gmail.com>
2026-09-05 02:13:12 +0530

    Add files via upload

---
+++ SVVLK/sw.js
@@ -0,0 +1,7 @@
+﻿self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+  // We just let the network handle it, this is a minimal SW just to trigger the "Install App" prompt
+});
